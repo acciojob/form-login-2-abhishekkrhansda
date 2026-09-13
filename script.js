e423d14b9c28d9035fe947c7faf6b7cf641dc0e2
abhishek kumar hansda <118197040+abhishekkrhansda@users.noreply.github.com>
2026-09-13 18:00:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let fname = document.querySelector("#fname");
+let lname = document.querySelector("#lname");
+let pnumber = document.querySelector("#pnumber");
+let email = document.querySelector("#email");
+
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click",() =>{
+	alert(
+    "First Name :" + fname.value + " " +
+    "\n Last Name :" + lname.value + " " +
+    "\n Phone Number :" + pnumber.value + " " +
+    "\n Email :" + email.value
+);
+})
